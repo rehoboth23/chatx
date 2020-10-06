ef740fd5ff7378af8b5b196139ba24f21c07c727
Rehoboth Okorie <65668510+rehoboth23@users.noreply.github.com>
2020-10-06 02:43:55 -0400

    Update settings.py

--- bant/bant/settings.py
+++ bant/bant/settings.py
@@ -152,12 +152,12 @@ USE_TZ = True
 # set up s3 bucket variable in environment
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = "test-mykc-bucket"
+AWS_STORAGE_BUCKET_NAME = "your-bucket-name"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'static'
 
-AWS_S3_REGION_NAME = 'us-east-2'  # change to your bucket region
+AWS_S3_REGION_NAME = 'your-bucket-region'  # change to your bucket region
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
