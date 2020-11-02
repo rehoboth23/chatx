a57987e57d1bcf495cb07b610f834379a064e1bd
Rehoboth Okorie <65668510+rehoboth23@users.noreply.github.com>
2020-11-02 18:26:12 -0500

    Update incoming.js

--- bant/chatx/src/components/chatMain/incoming.js
+++ bant/chatx/src/components/chatMain/incoming.js
@@ -1,21 +1,21 @@
 import React from "react";
 const date_handler = require("../../dateHandler")
 
-function Incoming(props){
+function Outgoing(props){
     const data = props.props
-    const picurl = `http://localhost:8000/${data['pic_url']}`
+    const picurl = `http://localhost:8000${data['pic_url']}`
     // const picurl = `${data['pic_url']}`
     return(
-        <div className="row incoming msg-holder mb-2">
+        <div className="row outgoing msg-holder mb-2 justify-content-end ml-auto">
+            <div className="text-holder">
+                <p className="rounded-left">{data['memo']}
+                <br/><span className="message-time">{date_handler(data.date)}</span>
+                </p>
+            </div>
             <div className="msg-toon-holder">
                 <img src={picurl} alt="toon-img" className="msg-toon rounded-circle"/>
-            </div>
-            <div className="text-holder">
-                <p className="rounded-lg">{data['memo']}
-                    <br/><span className="message-time">{date_handler(data.date)}</span>
-                </p>
             </div>
         </div>
     )
 }
-export default Incoming
+export default Outgoing
